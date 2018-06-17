c245186805587c20ad546a33823e37c43c472ebb
kristofferkarlsson93 <kristoffer.karlsson93@hotmail.com>
2018-06-18 00:40:09 +0200

    Prod-build for testing

--- config/index.js
+++ config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../flyttbrev/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../flyttbrev'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/flyttbrev/',
 
     /**
      * Source Maps
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
+  base: '/flyttbrev/',
   routes: [
     {
       path: '/',
